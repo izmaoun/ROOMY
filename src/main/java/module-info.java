26db58155a8b1d58f07a2c32e8dba33c60e0424d
izmaoun <a.izmaoun0773@uca.ac.ma>
2025-11-20 15:04:38 +0100

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module com.roomy {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.bootstrapfx.core;
+    requires com.almasb.fxgl.all;
+
+    opens com.roomy to javafx.fxml;
+    exports com.roomy;
+}
